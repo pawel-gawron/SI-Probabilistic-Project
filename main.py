37b2dd14daab74116cad979daa4d87fbae7ecdf6
pawel-gawron <gawron.pawel1999@gmail.com>
2023-05-24 19:47:19 +0200

    cut BB

--- main.py
+++ main.py
@@ -2,13 +2,42 @@ from pgmpy.models import BayesianModel
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
 from pgmpy.factors.discrete import JointProbabilityDistribution
-import numpy as np
 from pgmpy.factors.discrete import JointProbabilityDistribution as JPD
 
 from pathlib import Path
+import numpy as np
 import cv2
 import os
 import argparse
+
+def computeProbability(imagePath, boundingBoxPath):
+    imagesPath = imagePath
+    boundingBoxFile = boundingBoxPath
+
+    for i in range(len(imagesPath)):
+        coordinatesBoundingBoxes = []
+        imageName = boundingBoxFile.readline().rstrip("\n")
+        image = cv2.imread(str(imagePath[i]))
+
+
+        # print(imageName)
+
+        if not imageName:
+            break
+
+        boundingBoxNumber = boundingBoxFile.readline().rstrip("\n")
+        # print(boundingBoxNumber)
+
+        for bb in range(int(boundingBoxNumber)):
+            coordinates = boundingBoxFile.readline().rstrip("\n").split(" ")
+            coordinatesBoundingBoxes.append(coordinates)
+            x = int(float(coordinates[0]))
+            y = int(float(coordinates[1]))
+            w = int(float(coordinates[2]))
+            h = int(float(coordinates[3]))
+            cv2.imshow("image", image[y:y+h, x:x+w])
+            cv2.waitKey(0)
+            cv2.destroyAllWindows
 
 
 if __name__ == '__main__':
@@ -23,8 +52,11 @@ if __name__ == '__main__':
 
     imagesPath = sorted([image_path for image_path in imagesDir.iterdir() if image_path.name.endswith('.jpg')])
 
-    for image_path in imagesPath:
-        image = cv2.imread(str(image_path))
-        if image is None:
-            print(f'Error loading image {image_path}')
-            continue
+    file = open(boundingBoxDir, 'r')
+
+    computeProbability(imagesPath, file)
+    # for image_path in imagesPath:
+    #     image = cv2.imread(str(image_path))
+    #     if image is None:
+    #         print(f'Error loading image {image_path}')
+    #         continue
