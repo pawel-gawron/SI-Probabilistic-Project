adc9b0d2945f5bc523c33ceafbda1a93789f303a
pawel-gawron <gawron.pawel1999@gmail.com>
2023-05-25 23:45:47 +0200

    working HSV hist

--- main.py
+++ main.py
@@ -29,17 +29,17 @@ def calcHistBoundBox(boundBox):
     histV = cv2.calcHist([boundBox],[2], None, [v_bins], v_ranges, accumulate=False)
     cv2.normalize(histV, histV, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
 
-    # plt.plot(histH,color = 'red')
-    # plt.plot(histS,color = 'green')
-    # plt.plot(histV,color = 'blue')
-    # boundBox_bgr = cv2.cvtColor(boundBox, cv2.COLOR_HSV2BGR)
-    # plt.imshow(boundBox_bgr)
-    # plt.axis('off')
-    # plt.show()
-    # cv2.imshow("image", boundBox_bgr)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows
-    # plt.show()
+    plt.plot(histH,color = 'red')
+    plt.plot(histS,color = 'green')
+    plt.plot(histV,color = 'blue')
+    boundBox_bgr = cv2.cvtColor(boundBox, cv2.COLOR_HSV2BGR)
+    plt.imshow(boundBox_bgr)
+    plt.axis('off')
+    plt.show()
+    cv2.imshow("image", boundBox_bgr)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows
+    plt.show()
 
     boundBox_rgb = cv2.cvtColor(boundBox, cv2.COLOR_HSV2RGB)
     plt.imshow(boundBox_rgb)
@@ -66,7 +66,8 @@ def computeProbability(imagePath, boundingBoxPath):
         coordinatesBoundingBoxes = []
         histograms = []
         imageName = boundingBoxFile.readline().rstrip("\n")
-        image = cv2.imread(str(imagePath[imageNumber]), cv2.COLOR_HSV2BGR)
+        image = cv2.imread(str(imagePath[imageNumber]))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
 
         # print(imageName)
@@ -86,28 +87,7 @@ def computeProbability(imagePath, boundingBoxPath):
             y = int(float(coordinates[1]))
             w = int(float(coordinates[2]))
             h = int(float(coordinates[3]))
-            # histograms.append(calcHistBoundBox(image[y:y+h, x:x+w]))
-
-            h_ranges = [0, 180]
-            s_ranges = [0, 256]
-            v_ranges = [0, 256]
-
-            h_bins = 50
-            s_bins = 60
-            v_bins = 60
-
-            histH = cv2.calcHist([image[y:y+h, x:x+w]],[0], None, [h_bins], h_ranges, accumulate=False)
-            cv2.normalize(histH, histH, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
-
-            plt.plot(histH,color = 'red')
-            # print(cv2.cvtColor(image[y:y+h, x:x+w], cv2.COLOR_HSV2RGB))
-            plt.show()
-
-            # plt.imshow(image[y:y+h, x:x+w])
-            # plt.axis('off')
-            # plt.show()
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows
+            histograms.append(calcHistBoundBox(image[y:y+h, x:x+w]))
 
 
 if __name__ == '__main__':
