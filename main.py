bbc65d2b8a3dd92b0e2a04b576de933840f46893
pawel-gawron <gawron.pawel1999@gmail.com>
2023-05-30 07:49:30 +0200

    clear code

--- main.py
+++ main.py
@@ -32,58 +32,22 @@ def calcHistBoundBox(boundBox):
     histV = cv2.calcHist([boundBox],[2], None, [v_bins], v_ranges, accumulate=False)
     cv2.normalize(histV, histV, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32FC1)
 
-    # plt.plot(histH,color = 'red')
-    # plt.plot(histS,color = 'green')
-    # plt.plot(histV,color = 'blue')
-    # boundBox_bgr = cv2.cvtColor(boundBox, cv2.COLOR_HSV2BGR)
-    # plt.imshow(boundBox_bgr)
-    # plt.axis('off')
-    # plt.show()
-    # cv2.imshow("image", boundBox_bgr)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows
-    # plt.show()
-
-    # boundBox_rgb = cv2.cvtColor(boundBox, cv2.COLOR_HSV2RGB)
-    # plt.imshow(boundBox_rgb)
-    # plt.axis('off')
-    # plt.show()
-
     return [histH, histS]
 
 def compareHistBoundBox(boundBoxesCurrentHist, boundBoxesPreviousHist, factorGraph, newObjProb):
-    # print("boundBoxesPreviousHist: ", len(boundBoxesPreviousHist))
-    # print("boundBoxesCurrentHist: ", len(boundBoxesCurrentHist))
-    # print("boundBoxesPreviousHist: ", len(boundBoxesPreviousHist))
 
     for counterCurr, histCurr in enumerate(boundBoxesCurrentHist):
         similarityVect = []
-        # print("i: ", counterCurr)
         for counterPrev, histPrev in enumerate(boundBoxesPreviousHist):
-    # for i in range(len(boundBoxesCurrentHist)):
-    #     for j in range(len(boundBoxesPreviousHist)):
-            # print("boundBoxesCurrentHist[0]: ", histCurr)
-            # histCompH = cv2.compareHist(boundBoxesPreviousHist[i][0], boundBoxesCurrentHist[j][0], cv2.HISTCMP_BHATTACHARYYA)
-            # histCompS = cv2.compareHist(boundBoxesPreviousHist[i][1], boundBoxesCurrentHist[j][1], cv2.HISTCMP_BHATTACHARYYA)
-            # histCompV = cv2.compareHist(boundBoxesPreviousHist[i][2], boundBoxesCurrentHist[j][2], cv2.HISTCMP_BHATTACHARYYA)
             histCompH = cv2.compareHist(histPrev[0], histCurr[0], cv2.HISTCMP_CORREL)
             histCompS = cv2.compareHist(histPrev[1], histCurr[1], cv2.HISTCMP_CORREL)
-            # histCompV = cv2.compareHist(histPrev[2], histCurr[2], cv2.HISTCMP_CORREL)
-            # plt.plot(histPrev,color = 'blue')
-            # plt.show()
 
             similarity = (histCompH + histCompS)/2
             if similarity <= 0.0:
                 similarity = 0.01
             similarityVect.append(similarity)
-            # print(histCompH)
-            # print("[histPrev[0]]: ", histPrev[0])
-        # print(len(boundBoxesPreviousHist) + 1)
-        # print([[0.3] + similarityVect])
+
         factor = DiscreteFactor([str(counterCurr)], [len(boundBoxesPreviousHist) + 1], [[newObjProb] + similarityVect])
-        # print("factor: ", factor)
-        # print(factor)
-        # print("counterCurr:", counterCurr)
         factorGraph.add_factors(factor)
         factorGraph.add_edge(str(counterCurr),factor)
 
@@ -106,8 +70,6 @@ def computeProbability(imagePath, boundingBoxPath):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
         boundingBoxNumber = boundingBoxFile.readline().rstrip("\n")
-        # print(imageName)
-        # print("boundingBoxNumber: ", boundingBoxNumber)
 
         histogramsPrevious = histogramsCurrent
         boundingBoxNumberPrev = boundingBoxNumber
@@ -141,8 +103,6 @@ def computeProbability(imagePath, boundingBoxPath):
 
         nodesPossibilityMatrix = [0 if k == l else nodesPossibilityMatrix[k][l] for k in range(matrixSize) for l in range(matrixSize)]
         nodesPossibilityMatrix[0][0] = 1
-
-        # print("matrixSize: ", matrixSize)
 
         if histogramsPrevious != 0:
             compareHistBoundBox(histogramsCurrent, histogramsPrevious, factorGraph, probNewObject)
